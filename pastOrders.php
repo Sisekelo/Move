1e2286d07e148566f2a406c997f637df985e3b21
Sisekelo <sidlamini15@alustudent.com>
2017-12-20 11:30:47 +0200

    a

--- pastOrders.php
+++ pastOrders.php
@@ -1,6 +1,8 @@
 <?php
     	$Number= $_GET["number"];
     	$NumberPlus = '+'.$Number;
+    	$NumberPlus = str_replace(' ', '', $NumberPlus);
+
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
